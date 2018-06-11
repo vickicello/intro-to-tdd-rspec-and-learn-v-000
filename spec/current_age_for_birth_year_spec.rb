de7c5e860cb16ed9bc247bc5d49bbf81f25df553
Vicki Hamm <vicki.cello@gmail.com>
2018-06-11 19:44:55 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -5,7 +5,7 @@ describe "current_age_for_birth_year method" do
     current_year = Time.now.year
     birth_year = 1984
     answer = current_year - birth_year
- 
+
     age_of_person = current_age_for_birth_year(birth_year)
     expect(age_of_person).to eq(answer)
   end
